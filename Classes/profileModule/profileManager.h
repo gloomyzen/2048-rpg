15454892fa1883af2b55c96d528bc861cb72ef2d
Artur Barilo <faustkeller45@gmail.com>
2021-01-11 00:49:59 +0900

    map_tool | wip profile manager

--- Classes/profileModule/profileManager.h
+++ Classes/profileModule/profileManager.h
@@ -1,6 +1,11 @@
 #ifndef SWIPE_RPG_PROFILEMANAGER_H
 #define SWIPE_RPG_PROFILEMANAGER_H
 
+#include "profileModule/profileBlockInterface.h"
+#include <map>
+#include <string>
+#include <functional>
+
 namespace sr {
 	namespace profileModule {
 
@@ -9,6 +14,9 @@ namespace sr {
 			profileManager();
 			~profileManager();
 			static profileManager &getInstance();
+
+		private:
+			std::map<std::string, std::function<void()>> profileBlocks;
 		};
 	}
 }
