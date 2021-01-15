fcdad4a4d1e123680934f2cab2090949dddaecc0
Artur Barilo <faustkeller45@gmail.com>
2021-01-15 22:12:35 +0900

    (feature)map_scene_update* | wip map scene

--- Classes/mapModule/scrollHolder.cpp
+++ Classes/mapModule/scrollHolder.cpp
@@ -12,7 +12,7 @@ using namespace cocos2d;
 
 scrollHolder::scrollHolder() {
 	this->setName("scrollHolder");
-	loadProperty("mapScene/" + this->getName(), this);
+	loadProperty(this->getName(), this);
 }
 
 scrollHolder::~scrollHolder() {}
@@ -20,7 +20,7 @@ scrollHolder::~scrollHolder() {}
 std::deque<nodeTasks> scrollHolder::getTasks() {
 	std::deque<nodeTasks> result;
 	result.emplace_back([this]() {
-		CocosDenshion::SimpleAudioEngine::getInstance()->playBackgroundMusic("sounds/menu.wav", true);
+//		CocosDenshion::SimpleAudioEngine::getInstance()->playBackgroundMusic("sounds/menu.wav", true);
 		scrollView = dynamic_cast<ui::ScrollView*>(findNode("scrollContainer"));
 		//todo scroll by progress
 //		auto bg = dynamic_cast<Sprite*>(findNode("bg"));
