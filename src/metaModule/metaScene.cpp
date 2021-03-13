283697d86eb032f6c7079a509a8b662b3f94ec22
Artur Barilo <faustkeller45@gmail.com>
2021-03-14 01:33:23 +0900

    (feature)master | wip metaScene - remove shaders

--- src/metaModule/metaScene.cpp
+++ src/metaModule/metaScene.cpp
@@ -7,11 +7,4 @@ using namespace sr::metaModule;
 metaScene::metaScene() {
     this->setName("metaScene");
     loadProperty("scenes/" + this->getName(), dynamic_cast<Node*>(this));
-    auto vertFilename = std::string("shaders/shader").append(".vsh");
-    auto fragFilename = std::string("shaders/shader").append(".fsh");
-    auto glprogram = GLProgram::createWithFilenames(vertFilename, fragFilename);
-    GLProgramState* glprogramstate = GLProgramState::getOrCreateWithGLProgram(glprogram);
-
-    auto winsize = Director::getInstance()->getWinSizeInPixels();
-    glprogramstate->setUniformVec2("cc_ViewSizeInPixels", winsize);
 }
