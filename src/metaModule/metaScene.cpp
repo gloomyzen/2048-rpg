4107d257c45e9a065706075b40feb1cdbd61d462
Artur Barilo <faustkeller45@gmail.com>
2021-03-13 23:33:23 +0900

    (feature)master | wip metaScene - test shaders

--- src/metaModule/metaScene.cpp
+++ src/metaModule/metaScene.cpp
@@ -1,4 +1,5 @@
 #include "metaScene.h"
+#include <string>
 
 using namespace sr::metaModule;
 
@@ -6,4 +7,11 @@ using namespace sr::metaModule;
 metaScene::metaScene() {
     this->setName("metaScene");
     loadProperty("scenes/" + this->getName(), dynamic_cast<Node*>(this));
+    auto vertFilename = std::string("shaders/shader").append(".vsh");
+    auto fragFilename = std::string("shaders/shader").append(".fsh");
+    auto glprogram = GLProgram::createWithFilenames(vertFilename, fragFilename);
+    GLProgramState* glprogramstate = GLProgramState::getOrCreateWithGLProgram(glprogram);
+
+    auto winsize = Director::getInstance()->getWinSizeInPixels();
+    glprogramstate->setUniformVec2("cc_ViewSizeInPixels", winsize);
 }
